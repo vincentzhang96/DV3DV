1be1767d453c26b9e994503aae3afce020430129
Vincent Zhang <vzhang@purdue.edu>
2017-01-11 15:58:46 -0500

    Define more PPAC structs

--- source/ppac/PhoenixPAC.h
+++ source/ppac/PhoenixPAC.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <cstddef>
 
 namespace PPAC
 {
@@ -7,11 +8,32 @@ namespace PPAC
 	typedef uint16_t uint16;
 	typedef uint32_t uint32;
 	typedef uint64_t uint64;
+	typedef uint32_t GUARD;
 #ifdef PPAC_WIDE
-	typedef uint64 WIDE;
+	typedef uint64 OFFSET;
 #else
-	typedef uint32 WIDE;
+	typedef uint32 OFFSET;
 #endif
+
+#define PPAC_HEAD_MAGIC 0x50504143
+#define PPAC_INDEX_GUARD 0x494E4458
+#define PPAC_META_GUARD 0x4D455441
+#define PPAC_TRASH_GUARD 0x54525348
+#define CHECK_HEAD_MAGIC(head) (head.hMagic == PPAC_HEAD_MAGIC)
+#define CHECK_INDEX_GUARD(index) (index.iGuard == PPAC_INDEX_GUARD)
+#define CHECK_META_GUARD(meta) (meta.mGuard == PPAC_META_GUARD)
+#define CHECK_TRASH_GUARD(trash) (trash.iGuard == PPAC_TRASH_GUARD)
+
+	//	Forward declarations
+	union TPUID;
+	struct PPACHEADER;
+	struct PPACINDEX;
+	struct PPACINDEXENTRY;
+	struct PPACMETA;
+	struct PPACMETABLOCK;
+	struct PPACMETABLOCKENTRY;
+	struct PPACTRASHINDEX;
+	struct PPACTRASHINDEXENTRY;
 
 	typedef union TPUID
 	{
@@ -31,8 +53,63 @@ namespace PPAC
 		uint16 hMinorVer;
 		uint8 hReserved[16];
 		uint32 hFlags;
-		WIDE hIndexOffset;
-		WIDE hMetadataOffset;
-		WIDE hTrashIndexOffset;
+		OFFSET hIndexOffset;
+		OFFSET hMetadataOffset;
+		OFFSET hTrashIndexOffset;
 	} PPACHEADER;
+
+	typedef struct PPACINDEX
+	{
+		uint32 iCount;
+		PPACINDEXENTRY* iEntries;
+		GUARD iGuard;
+	} PPACINDEX;
+
+	typedef struct PPACINDEXENTRY
+	{
+		TPUID ieTPUID;
+		OFFSET ieDiskOffset;
+		uint32 ieDiskSize;
+		uint32 ieMemorySize;
+		uint8 ieCompressionType;
+		uint8 ieReserved[3];
+		uint8 ieSHA256;
+	} PPACINDEXENTRY;
+
+	typedef struct PPACMETA
+	{
+		uint32 mSize;
+		uint32 mCount;
+		PPACMETABLOCK* mBlocks;
+		GUARD mGuard;
+	} PPACMETA;
+
+	typedef struct PPACMETABLOCK
+	{
+		TPUID mbTPUID;
+		uint16 mbCount;
+		uint16 mbSize;
+		PPACMETABLOCKENTRY* mbEntries;
+	} PPACMETABLOCK;
+
+	typedef struct PPACMETABLOCKENTRY
+	{
+		uint8 mbeKeySize;
+		uint8 mbeValSize;
+		uint8* mbeKey;
+		uint8* mbeVal;
+	} PPACMETABLOCKENTRY;
+
+	typedef struct PPACTRASHINDEX
+	{
+		uint32 tCount;
+		PPACTRASHINDEXENTRY* tEntries;
+		GUARD tGuard;
+	} PPACTRASHINDEX;
+
+	typedef struct PPACTRASHINDEXENTRY
+	{
+		OFFSET teOffset;
+		uint32 teSize;
+	} PPACTRASHINDEXENTRY;
 }
