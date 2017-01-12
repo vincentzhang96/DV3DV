e99adc1f2687143e77b04b3dc3463d4d1df22861
Vincent Zhang <vzhang@purdue.edu>
2017-01-12 13:08:41 -0500

    Switch target ver to 8.1

--- source/targetver.h
+++ source/targetver.h
@@ -5,8 +5,9 @@
 // If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
 // set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 
-#define WINVER 0x0601
-#define _WIN32_WINNT_WIN7 0x0601
-#define _WIN32_WINNT 0x0601
+//	Target Windows 8.1 (backwards compatible with 7)
+#define WINVER 0x0603
+#define _WIN32_WINNT_WINBLUE 0x0603 // Windows 8.1  
+#define _WIN32_WINNT 0x0603
 
 #include <SDKDDKVer.h>
