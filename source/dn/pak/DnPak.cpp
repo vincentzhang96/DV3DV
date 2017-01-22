151c04d5803a00423f1653ae0b6e59853cc85be2
Vincent Zhang <vzhang@purdue.edu>
2017-01-22 18:19:53 -0500

    Implement indexing of PAK files

--- source/dn/pak/DnPak.cpp
+++ source/dn/pak/DnPak.cpp
@@ -2,6 +2,15 @@
 
 using namespace dn;
 
+PakFileTableEntry::PakFileTableEntry()
+{
+}
+
+PakFileTableEntry::PakFileTableEntry(void* ignored)
+{
+}
+
+
 OPENPAKFILEHANDLE::OPENPAKFILEHANDLE(HANDLE handle, std::wstring name)
 {
 	_handle = handle;
@@ -10,29 +19,130 @@ OPENPAKFILEHANDLE::OPENPAKFILEHANDLE(HANDLE handle, std::wstring name)
 
 OPENPAKFILEHANDLE::~OPENPAKFILEHANDLE()
 {
+	CLOG(DEBUG, DNPAKLOGGER) << "Closing handle " << _name;
 	CloseHandle(_handle);
-	std::cout << "Closing handle " << _name.c_str();
 }
 
 cPakData::cPakData(std::string name, uint32 size)
 {
+	_name = name;
+	_size = size;
+	_data.reserve(_size);
 }
 
 cPakData::~cPakData()
 {
 }
 
-bool cPak::_ReadHeader(const std::wstring file)
+int dn::_memcpyIncr(void* dest, const void* src, const size_t sz)
 {
-	return false;
+	memcpy(dest, src, sz);
+	return sz;
 }
 
-void cPak::_ReadIndex(const bool needSwp, const std::wstring file)
+#define READ_FIELD_INCR(field, buffer) buffer += dn::_memcpyIncr(&field, buffer, sizeof(field))
+
+void cPak::_ReadHeader(const std::wstring file)
 {
+	std::unique_ptr<uint8[]> buffer(new uint8[szPakHeader]);
+	DWORD dwBytesRead;
+	BOOL bFlag = ReadFile(_handle.get()->_handle, buffer.get(), szPakHeader, &dwBytesRead, nullptr);
+	if (bFlag == FALSE)
+	{
+		CLOG(WARNING, DNPAKLOGGER) << "Failed to read file " << file << " " << GetLastError();
+		throw "Unable to read file";
+	}
+	if (dwBytesRead != szPakHeader)
+	{
+		CLOG(WARNING, DNPAKLOGGER) << "Incomplete read of file " << file;
+		throw "Incomplete read";
+	}
+	auto bufPtr = buffer.get();
+	READ_FIELD_INCR(_header.hMagic, bufPtr);
+	READ_FIELD_INCR(_header.hVersion, bufPtr);
+	if (_header.hVersion != 11)
+	{
+		CLOG(WARNING, DNPAKLOGGER) << "Unsupported version " << _header.hVersion;
+		throw "Unsupported version";
+	}
+	READ_FIELD_INCR(_header.hFileCount, bufPtr);
+	READ_FIELD_INCR(_header.hFileTableOffset, bufPtr);
+
+}
+
+void cPak::_ReadIndex(const std::wstring file)
+{
+	LONG lDistanceToMove = _header.hFileTableOffset;
+	LONG lDistanceToMoveHigh = 0L;
+	if (SetFilePointer(_handle.get()->_handle, lDistanceToMove, &lDistanceToMoveHigh, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
+	{
+		CLOG(WARNING, DNPAKLOGGER) << "Failed to read file " << file << " SetFilePointer " << GetLastError();
+		throw "Unable to read file";
+	}
+	size_t tableLen = szPakFileTableEntry * _header.hFileCount;
+	_fileTable.reserve(_header.hFileCount);
+	std::unique_ptr<uint8[]> buffer(new uint8[tableLen]);
+	DWORD dwBytesRead;
+	BOOL bFlag = ReadFile(_handle.get()->_handle, buffer.get(), tableLen, &dwBytesRead, nullptr);
+	if (bFlag == FALSE)
+	{
+		CLOG(WARNING, DNPAKLOGGER) << "Failed to read file " << file << " " << GetLastError();
+		throw "Unable to read file";
+	}
+	if (dwBytesRead != tableLen)
+	{
+		CLOG(WARNING, DNPAKLOGGER) << "Incomplete read of file " << file;
+		throw "Incomplete read";
+	}
+	auto bufPtr = buffer.get();
+	char strBuf[256];
+	for (auto i = 0U; i < _header.hFileCount; ++i)
+	{
+		auto strt = bufPtr;
+		bufPtr += dn::_memcpyIncr(strBuf, bufPtr, 256);
+		std::string path(strBuf);
+		std::transform(path.begin(), path.end(), path.begin(), ::tolower);
+		auto ret = _fileTable.emplace(path, nullptr);
+		if (ret.second)
+		{
+			auto entryIter = ret.first;
+			auto entry = entryIter->second;
+			entry.eFilePath = entryIter->first;
+			READ_FIELD_INCR(entry.eDiskSize, bufPtr);
+			READ_FIELD_INCR(entry.eDecompressedSize, bufPtr);
+			READ_FIELD_INCR(entry.eCompressedSize, bufPtr);
+			READ_FIELD_INCR(entry.eDataOffset, bufPtr);
+			READ_FIELD_INCR(entry.eUnknownA, bufPtr);
+			READ_FIELD_INCR(entry.eReserved, bufPtr);
+		}
+		else
+		{
+			bufPtr += szPakFileTableEntry - 256;
+		}
+	}
 }
 
 cPak::cPak(LPCWSTR file)
 {
+	CLOG(DEBUG, DNPAKLOGGER) << "Reading file " << file;
+	HANDLE hFile = CreateFileW(file,
+		GENERIC_READ,
+		FILE_SHARE_READ,
+		nullptr,
+		OPEN_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		nullptr);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		CLOG(WARNING, DNPAKLOGGER) << "Failed to open handle to file " << file;
+		throw "Unable to open file";
+	}
+	_handle = std::make_unique<OPENPAKFILEHANDLE>(hFile, file);
+
+	//	READ HEADER
+	_ReadHeader(file);
+	//	READ INDEX
+	_ReadIndex(file);
 }
 
 std::unique_ptr<cPakData> cPak::GetFileData(const std::string fileName)
--- source/dn/pak/DnPak.h
+++ source/dn/pak/DnPak.h
@@ -10,6 +10,11 @@ namespace dn
 	typedef uint32_t uint32;
 	typedef uint64_t uint64;
 
+#define DNPAKLOGGER "dn-pak"
+#define INIT_DNPAK_LOGGER el::Logger* dnPakLogger = el::Loggers::getLogger("dn-pak");
+
+
+
 	struct PakHeader;
 	struct PakFileTableEntry;
 	class OPENPAKFILEHANDLE;
@@ -18,22 +23,32 @@ namespace dn
 
 	typedef struct PakHeader
 	{
-		uint8 hMagic[1024];
+		uint8 hMagic[256];
 		uint32 hVersion;
 		uint32 hFileCount;
 		uint32 hFileTableOffset;
 	} PakHeader;
 
+	const size_t szPakHeader = sizeof(uint8) * 256 +
+		sizeof(uint32) * 3;
+
 	typedef struct PakFileTableEntry
 	{
-		uint8 eFilePath[256];
+		std::string eFilePath;
 		uint32 eDiskSize;
 		uint32 eDecompressedSize;
 		uint32 eCompressedSize;
 		uint32 eDataOffset;
 		uint32 eUnknownA;
 		uint8 eReserved[40];
+
+		PakFileTableEntry();
+		PakFileTableEntry(void* ignored);
 	} PakFileTableEntry;
+
+	const size_t szPakFileTableEntry = 256 +
+		sizeof(uint32) * 5 +
+		sizeof(uint8) * 40;
 
 	class OPENPAKFILEHANDLE
 	{
@@ -56,14 +71,15 @@ namespace dn
 
 	class cPak
 	{
+		typedef std::unordered_map<std::string, PakFileTableEntry> PakFileTable;
 	private:
 		PakHeader _header;
-		std::vector<PakFileTableEntry> _fileTable;
+		PakFileTable _fileTable;
 		std::unique_ptr<OPENPAKFILEHANDLE> _handle;
 		std::mutex _readMutex;
 
-		bool _ReadHeader(const std::wstring file);
-		void _ReadIndex(const bool needSwp, const std::wstring file);
+		void _ReadHeader(const std::wstring file);
+		void _ReadIndex(const std::wstring file);
 
 	public:
 		explicit cPak(LPCWSTR file);
@@ -73,4 +89,6 @@ namespace dn
 
 		std::vector<PakFileTableEntry> GetEntries();
 	};
+
+	int _memcpyIncr(void* dest, const void* src, const size_t sz);
 }
