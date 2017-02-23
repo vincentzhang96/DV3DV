44212c65b056b551e8e8b22ac7f60531e1502614
Vincent Zhang <vzhang@purdue.edu>
2017-02-23 11:17:15 -0500

    Add basic loading of shared ui items

--- source/ui/UiBootstrap.cpp
+++ source/ui/UiBootstrap.cpp
@@ -2,9 +2,26 @@
 #include "UiBootstrap.h"
 
 
+MainMenuSharedResources::MainMenuSharedResources(dv3d::TextureManager* texMan) :
+_texMan(texMan)
+{
+	backgroundTex = 0;
+	invVignetteTex = 0;
+	vignetteTex = 0;
+}
+
+MainMenuSharedResources::~MainMenuSharedResources()
+{
+	LOG(DEBUG) << "Unloading shared main menu resources";
+	_texMan->Unload(backgroundTex);
+	_texMan->Unload(invVignetteTex);
+	_texMan->Unload(vignetteTex);
+}
+
 UiBootstrap::UiBootstrap(DivinitorApp* app) :
 	UIScreen(app)
-{;
+{
+	_sharedResources = std::make_shared<MainMenuSharedResources>(app->_textureManager);
 }
 
 UiBootstrap::~UiBootstrap()
@@ -14,6 +31,9 @@ UiBootstrap::~UiBootstrap()
 void UiBootstrap::Init()
 {
 	_ui->InvalidateOldScreen();
+	_sharedResources->backgroundTex = _app->_textureManager->LoadAndGet(ppac::TPUID(0x0206, 0x1001, 0x00000001)).second;
+	_sharedResources->vignetteTex = _app->_textureManager->LoadAndGet(ppac::TPUID(0x0206, 0x1001, 0x00000002)).second;
+	_sharedResources->invVignetteTex = _app->_textureManager->LoadAndGet(ppac::TPUID(0x0206, 0x1001, 0x00000003)).second;
 }
 
 void UiBootstrap::Draw(float deltaT)
@@ -23,7 +43,7 @@ void UiBootstrap::Draw(float deltaT)
 
 	_text->DrawDynamicText2D(
 		_app->fhGeomanistRegular,
-		"DIVINITOR",
+		"TEST",
 		36,
 		10,
 		_ui->GetScreenSize().y - 46,
--- source/ui/UiBootstrap.h
+++ source/ui/UiBootstrap.h
@@ -1,15 +1,24 @@
 #pragma once
 #include "../UserInterface.h"
 
-struct MainMenuSharedResources
+class MainMenuSharedResources
 {
+	dv3d::TextureManager* _texMan;
+
+public:
 	dv3d::GLTEXHANDLE backgroundTex;
 	dv3d::GLTEXHANDLE vignetteTex;
 	dv3d::GLTEXHANDLE invVignetteTex;
+
+	explicit MainMenuSharedResources(dv3d::TextureManager* texMan);
+	~MainMenuSharedResources();
 };
 
 class UiBootstrap : public UIScreen
 {
+	typedef std::shared_ptr<MainMenuSharedResources> MainMenuSharedResourcesPtr;
+	MainMenuSharedResourcesPtr _sharedResources;
+
 public:
 	explicit UiBootstrap(DivinitorApp* app);
 
