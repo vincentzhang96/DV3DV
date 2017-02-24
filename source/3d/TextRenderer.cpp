adf92ded841ccdf00406999f6e2d210836465dbb
Vincent Zhang <vzhang@purdue.edu>
2017-02-24 10:19:04 -0500

    Zero out static text texture to remove artifacting

--- source/3d/TextRenderer.cpp
+++ source/3d/TextRenderer.cpp
@@ -412,6 +412,7 @@ dv3d::STATICTEXTHANDLE dv3d::TextRenderer::CreateStaticText(FONTHANDLE hFont, co
 	stext.textOptions = options;
 	glGenTextures(1, &stext.textTexture);
 	glBindTexture(GL_TEXTURE_2D, stext.textTexture);
+	std::vector<uint8_t> emptyBuf(stext.textWidth * stext.textHeight, 0);
 	glTexImage2D(GL_TEXTURE_2D,
 		0,
 		GL_RED,
@@ -420,7 +421,7 @@ dv3d::STATICTEXTHANDLE dv3d::TextRenderer::CreateStaticText(FONTHANDLE hFont, co
 		0,
 		GL_RED,
 		GL_UNSIGNED_BYTE,
-		nullptr
+		emptyBuf.data()
 	);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
