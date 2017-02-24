d18f5353aad21d621837acecd5a27039ac4d534e
Vincent Zhang <vzhang@purdue.edu>
2017-02-24 02:12:08 -0500

    Big spinner skeleton

---
+++ source/ui/uielement/UiBigSpinner.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../../UserInterface.h"
+
+namespace UiElements
+{
+	namespace MainMenu
+	{
+		class UiBigSpinner : public UiElement
+		{
+			explicit UiBigSpinner(UiScreen* parent) :
+				UiElement(parent)
+			{
+			}
+		};
+	}
+}
